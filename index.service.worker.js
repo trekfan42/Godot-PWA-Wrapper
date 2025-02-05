b73b7228fd9f2705cd7676e6675d327375230893
trekfan42 <davisam42@gmail.com>
2025-02-05 13:13:46 -0500

    test-ffmpeg

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738778095|3240963815';
+const CACHE_VERSION = '1738779200|59357458';
 /** @type {string} */
 const CACHE_PREFIX = 'a16z Kiosk-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

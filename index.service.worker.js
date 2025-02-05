b44583d0d8d381b474b4e995f03d9c684f3da808
trekfan42 <davisam42@gmail.com>
2025-02-05 12:55:05 -0500

    test-v6

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738777954|3100457527';
+const CACHE_VERSION = '1738778095|3240963815';
 /** @type {string} */
 const CACHE_PREFIX = 'a16z Kiosk-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

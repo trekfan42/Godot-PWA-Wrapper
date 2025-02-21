0921e47124d86fa90f818e599d52137cd6c08118
trekfan42 <davisam42@gmail.com>
2025-02-21 15:59:29 -0500

    bugs

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1740171027|2301635546";
+const CACHE_VERSION = "1740171559|2833818743";
 const CACHE_PREFIX = "a16z Kiosk-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";

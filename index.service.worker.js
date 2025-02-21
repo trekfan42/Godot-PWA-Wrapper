733036a5b69ec23f152eccf718f4e9c7fcad1d53
trekfan42 <davisam42@gmail.com>
2025-02-21 15:36:58 -0500

    custom urls
    Added custom url loading

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1738806947|55586828";
+const CACHE_VERSION = "1740170178|1452988479";
 const CACHE_PREFIX = "a16z Kiosk-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
